b81daf53d11dcf4ffc7c4fbb0b636860ace219db
conorbot <cwoodhayes@gmail.com>
2025-11-18 11:56:29 -0600

    added basic structure for FontTrajectory

---
+++ penpal/penpal/font_trajectory.py
@@ -0,0 +1,33 @@
+"""
+Generation of 2D trajectories (plus pressure) given font + text.
+"""
+
+import pathlib
+from dataclasses import dataclass
+from penpal.penpal.write_planner import WritePlanner
+
+
+class FontTrajectory:
+    """
+    Generates 2D trajectories + pressure given font & text.
+    """
+
+    @dataclass
+    class Config:
+        pass
+
+    def __init__(self, writer: WritePlanner, cfg: Config) -> None:
+        # self._fonts: dict[str, TTFont] = dict()
+        pass
+
+    def add_font(self, otf_path: pathlib.Path) -> None:
+        # load the font from the file
+        # and add it to self._fonts
+        pass
+
+    def write_text(
+        self, text: str, font_name: str, font_size_mm: float, pen_thickness_mm: float
+    ) -> None:
+        # get what font to use from
+        # self._fonts[font_name]
+        pass
